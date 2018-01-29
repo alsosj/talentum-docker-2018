51978d4803d8c54c962deac7b39021f97ec9920a
Álvaro San Juan Cervera <alsosj@gmail.com>
2018-01-29 17:35:13 +0100

    - Fixed requirements - Fixed settings - Added test case

--- src/blog/tests.py
+++ src/blog/tests.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
+from blog.models import Post
+
+
+class BlogTestCase(TestCase):
+	def setUp(self):
+		User.objects.create(
+			username='tester',
+			password='AsDf0987',
+			email='tester@gmail.com'
+		)
+
+	def create_post(self):
+		post_count = Post.objects.all().count()
+		Post.objects.create(
+			author=User.objects.first(),
+			title='Post de prueba',
+			text='Hola'
+		)
+		self.assertEquals(Post.objects.all().count(), post_count + 1)
--- src/taller_docker/settings/base.py
+++ src/taller_docker/settings/base.py
@@ -40,6 +40,13 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_flake8',
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
